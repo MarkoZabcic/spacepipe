10c285ce4672dc68a96ac4680cc33ef8b7f4cb1d
Pascal Kaufmann <kaufmae@me.com>
2017-05-08 16:37:11 +0200

    async await is beautiful

--- src/update-version-info.js
+++ src/update-version-info.js
@@ -1,59 +1,36 @@
-import { exec } from 'child_process';
-import Promise from 'promise';
+import { execAsync } from 'async-child-process';
+import { readFile, writeFile } from 'async-file';
 import path from 'path';
 import moment from 'moment';
-import fs from 'fs';
 
 const VERSION_FILE = 'private/version.json';
+const BUILD_NUMBER = process.env.npm_package_config_buildNumber;
 
-function getBranch() {
-  return new Promise((fulfill, reject) => {
-    exec(
-            "git branch | grep '*'",
-            (err, stdout) => {
-              if (err)reject(err);
-              const name = stdout.replace('* ', '').replace('\n', '');
-              fulfill(name);
-            },
-        );
-  });
+async function getBranch() {
+  const { stdout } = await execAsync("git branch | grep '*'");
+  return stdout.replace('* ', '').replace('\n', '');
 }
 
-function getCommit() {
-  return new Promise((fulfill, reject) => {
-    exec(
-            'git rev-parse HEAD',
-            (err, stdout) => {
-              if (err)reject(err);
-              const commitName = stdout.replace('* ', '').replace('\n', '');
-              fulfill(commitName);
-            },
-        );
-  });
+async function getCommit() {
+  const { stdout } = await execAsync('git rev-parse HEAD');
+  return stdout.replace('* ', '').replace('\n', '');
 }
 
 export default async function updateVersionInfo(args, callback) {
+  const currentDirectory = process.cwd();
+  const { version } = JSON.parse(await readFile('package.json', 'utf-8'));
+
   const result = {
     timestamp: moment().format('DD-MM-YYYY HH:mm'),
+    branch: await getBranch(),
+    commit: await getCommit(),
+    build: BUILD_NUMBER,
+    version,
   };
-  const currentDirectory = process.cwd();
-  return getBranch()
-      .then((_branch) => {
-        result.branch = _branch;
-      })
-      .then(getCommit)
-      .then((_commit) => {
-        result.commit = _commit;
-      })
-      .then(() => {
-        const packageContent = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
-        result.build = process.env.npm_package_config_buildNumber;
-        result.version = packageContent.version;
-        const fileContent = JSON.stringify(result, null, 2);
-        const pathToFile = path.normalize(`${currentDirectory}/${VERSION_FILE}`);
-        this.log(fileContent);
-        this.log(`path: ${pathToFile}`);
-        fs.writeFileSync(pathToFile, fileContent);
-        callback();
-      });
+
+  const fileContent = `${JSON.stringify(result, null, 2)}\n`;
+  const pathToFile = path.normalize(`${currentDirectory}/${VERSION_FILE}`);
+  this.log(`>(spacepipe)>: Updating version file (${pathToFile})\n${fileContent}`);
+  await writeFile(pathToFile, fileContent);
+  return callback();
 }
